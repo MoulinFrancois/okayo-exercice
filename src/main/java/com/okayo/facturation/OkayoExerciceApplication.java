29055cf48e719ddef6801b57054611346b754bc6
fmo <francois.moulin@gmail.com>
2026-01-09 18:20:29 +0100

    Creation projet

---
+++ src/main/java/com/okayo/facturation/OkayoExerciceApplication.java
@@ -0,0 +1,11 @@
+package com.okayo.facturation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkayoExerciceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OkayoExerciceApplication.class, args);
+    }
+}
